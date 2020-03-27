99aaa2f617aec52f33cfe3eb70c8433f8d807dbe
BenJ <benjamin.lousteau@etu.univ-amu.fr>
2020-03-27 15:16:51 +0100

    Classe JeuTest : Test TraiterCommande(), mais il manque SacADos et resoudreEnquete

--- src/tests/JeuTest.java
+++ src/tests/JeuTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import jeu.Jeu;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,8 +9,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class JeuTest {
 
+    Jeu jeu1;
+
     @BeforeEach
     void setUp() {
+        jeu1 = new Jeu();
     }
 
     @AfterEach
@@ -22,13 +26,21 @@ class JeuTest {
 
     @Test
     void traiterCommande() {
+        String cmd1 = "Oui";
+        String cmd2 = "OUI COUTEAU INDIGO";
+        String cmd3 = "OUI COUTEAU INDIGO OK OK";
+        assertFalse(cmd1.matches("OUI [A-Z]* [A-Z]*"));
+        assertTrue(cmd2.matches("OUI [A-Z]* [A-Z]*"));
+        assertFalse(cmd1.matches("OUI [A-Z]* [A-Z]*"));
     }
 
     @Test
     void resoudreEnquete() {
+
     }
 
     @Test
     void sacADos() {
+        //assertEquals("Il n'y a pas d'indice dans le sac à dos", SacADos());
     }
 }
