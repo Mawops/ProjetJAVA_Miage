137d4e186e25c29a4f3e099397bc456e9c6e93e0
flofli <floriane.bichyvagnol@gmail.com>
2020-03-27 11:52:15 +0100

    ajout du test jeu

---
+++ src/tests/JeuTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JeuTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void setGUI() {
+    }
+
+    @Test
+    void traiterCommande() {
+    }
+
+    @Test
+    void resoudreEnquete() {
+    }
+
+    @Test
+    void sacADos() {
+    }
+}
