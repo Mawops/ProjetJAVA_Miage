35c385e248cb0584b199f485deb0106cc9fe4b0c
Legendaire13 <45421561+Legendaire13@users.noreply.github.com>
2020-04-07 00:04:39 +0200

    ajout trouvedescription

--- src/jeu/Joueur.java
+++ src/jeu/Joueur.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Joueur extends Personnage {
 
-    private ArrayList<Indices> listeIndice;
+    private ArrayList<Indice> listeIndice;
 
     /**
      *Constructeur
@@ -14,7 +14,7 @@ public class Joueur extends Personnage {
     public Joueur(String pNom)
     {
         super(pNom);
-        listeIndice = new ArrayList<Indices>();
+        listeIndice = new ArrayList<Indice>();
     }
 
     /**
@@ -25,9 +25,28 @@ public class Joueur extends Personnage {
     public boolean trouverIndice(String nom)
     {
         boolean trouve = false;
-        for(Indices i : listeIndice)
+        for(Indice i : listeIndice)
         {
             if(i.getNom() == nom)
+            {
+                trouve = true;
+            }
+        }
+        return trouve;
+    }
+
+
+    /**
+     * recherche une description dans la liste
+     * @param nom : nom de la description
+     * @return true or false
+     */
+    public boolean trouveDescription(String nom)
+    {
+        boolean trouve = false;
+        for(Indice i : listeIndice)
+        {
+            if(i.getDescription() == nom)
             {
                 trouve = true;
             }
@@ -39,7 +58,7 @@ public class Joueur extends Personnage {
      * ajoute un nouvel indice dans la liste
      * @param i : indice à ajouter
      */
-    public void ajouterIndice(Indices i)
+    public void ajouterIndice(Indice i)
     {
         listeIndice.add(i);
     }
@@ -57,8 +76,8 @@ public class Joueur extends Personnage {
      * retourne la liste d'indice
      * @return
      */
-    public ArrayList<Indices> getIndice()
+    public ArrayList<Indice> getIndice()
     {
-    	return listeIndice;
+        return listeIndice;
     }
 }
